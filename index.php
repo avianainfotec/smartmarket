d054a7302da3cce420b1da9b94ca2d0f249dbf9d
Anderson A. Viana <avianainfotecaju@gmail.com>
2019-07-28 08:53:26 -0300

    Criamos o lin e logout com class model

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
+use Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -11,9 +11,48 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$Page = new Page();
+	$page = new Hcode\Page();
 
-	$Page->setTpl("index");
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	User::verifyLogin();
+
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
